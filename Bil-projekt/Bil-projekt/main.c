1abe7429995a123a5e3bfb5c1e08b82f3d633d41
Jacob Nilsson <nilssonjacob98@gmail.com>
2022-02-24 16:14:54 +0100

    Added Microchip files

---
+++ Bil-projekt/Bil-projekt/main.c
@@ -0,0 +1,12 @@
+#include <avr/io.h>
+
+
+int main()
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+	return 0;
+}
+
---
+++ Bil-projekt/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 2022-02-24 16:10:56
+ * Author : alfat
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
