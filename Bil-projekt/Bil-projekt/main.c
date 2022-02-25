84e0c045f93a6ed986d2f709cf28f6e615520762
Jacob Nilsson <nilssonjacob98@gmail.com>
2022-02-25 11:16:00 +0100

    Redigerade Daniels fel

--- Bil-projekt/Bil-projekt/main.c
+++ Bil-projekt/Bil-projekt/main.c
@@ -6,7 +6,7 @@ int main()
     /* Replace with your application code */
     while (1) 
     {
-		if (this->github= true) kiss me;
+		if (this->github == true) kiss me;
 			else fuck off;
     }
 	return 0;
