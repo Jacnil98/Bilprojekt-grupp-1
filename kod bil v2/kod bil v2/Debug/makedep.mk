f3f92a964f91746460ada3d6e15d1a46bb802c25
John Doe <daniel.mentzer@hotmail.com>
2022-04-02 20:40:34 +0200

    de ballar ur

---
+++ kod bil v2/kod bil v2/Debug/makedep.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+Button.cpp
+
+GPIO.cpp
+
+interrupts.cpp
+
+main.cpp
+
+Motor.cpp
+
+Serial.cpp
+
+Timer.cpp
+
+USART_Timer.cpp
+
---
+++ kod bil v2/kod bil v2/GPIO.cpp
@@ -0,0 +1,32 @@
+#include "GPIO.h"
+
+Motor motor;
+Button button;
+
+void init_GPIO()
+{
+	button = start_Button(13);
+	button.enable_interrupt();
+	motor = pwm_motor(5);
+	motor.enable_interrupt();
+	DDRD |= ((1<<IN1)|(1<<IN2));
+	return;
+}
+
+Button start_Button(const uint8_t PIN)
+{
+	Button self(PIN);
+	return self;
+}
+
+Motor pwm_motor(const uint8_t PIN)
+{
+	Motor self(PIN);
+	return self;
+}
+
+USART_Timer new_USART_Timer(const uint16_t delay)
+{
+	USART_Timer self(delay);
+	return self;
+}
