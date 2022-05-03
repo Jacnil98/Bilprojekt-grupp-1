a6a761d075a5e7caea88f186df237e151781f2f9
John Doe <daniel.mentzer@hotmail.com>
2022-05-03 15:28:18 +0200

    yes

--- C-kod motor/C-kod motor/src/header.h
+++ C-kod motor/C-kod motor/src/header.h
@@ -1,1 +1,15 @@
+<<<<<<< ours
 typedef enum {TRUE = 1, FALSE = 0} boolean;
+=======
+#include <avr/io.h>
+#include <math.h>
+#include <util/delay.h>
+#include <stdio.h>
+
+typedef enum {TRUE = 1, FALSE = 0} boolean;
+
+uint32_t ADC_read();
+{
+    return;
+}
+>>>>>>> theirs
