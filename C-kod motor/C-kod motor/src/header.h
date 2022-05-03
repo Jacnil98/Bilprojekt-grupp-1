9925f7a7b580718a91142d562b1e3cd2c42e832e
Jacob Nilsson <nilssonjacob98@gmail.com>
2022-05-03 15:33:53 +0200

    hej

--- C-kod motor/C-kod motor/src/header.h
+++ C-kod motor/C-kod motor/src/header.h
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< ours
-typedef enum {TRUE = 1, FALSE = 0} boolean;
-=======
-=======
->>>>>>> 80124f40ef7cf4833ee40a6f059307ddb05307fc
 #include <avr/io.h>
 #include <math.h>
 #include <util/delay.h>
@@ -11,12 +5,6 @@ typedef enum {TRUE = 1, FALSE = 0} boolean;
 
 typedef enum {TRUE = 1, FALSE = 0} boolean;
 
-<<<<<<< HEAD
+
 uint32_t ADC_read();
-{
-    return;
-}
->>>>>>> theirs
-=======
-uint32_t ADC_read();
->>>>>>> 80124f40ef7cf4833ee40a6f059307ddb05307fc
+
